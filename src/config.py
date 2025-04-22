bfcd4e397b3fc80baa3bf718e85879a5e9abdf70
Frank Joseph Lopez Cruz <79061389+FJLopez01@users.noreply.github.com>
2025-04-22 16:42:55 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,13 @@
 import os
 
 # Rutas
-BASE_PATH = r"C:\Users\SISTEMAS AUX\Desktop\Nominas" 
+BASE_PATH = r"C:\Tu\ubicacion\de\Nominas" 
 XML_PATH = os.path.join(BASE_PATH, "XML")
 PDF_PATH = os.path.join(BASE_PATH, "PDFs")
-EXCEL_CORREOS = os.path.join(BASE_PATH, "correos_colaboradores_prueba.xlsx")
+EXCEL_CORREOS = os.path.join(BASE_PATH, "correos_colaboradores.xlsx")
 
 # Configuración de correo
 EMAIL_SENDER = "tuusuario@gmail.com"
-EMAIL_PASSWORD = "pbbkwaodxelgffgz"
+EMAIL_PASSWORD = "tucontraseñasdeaplicaciones"
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
